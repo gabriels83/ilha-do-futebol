6c8bc58d3bb0ea6f41e86739ae13785fc6db887a
gabriels83 <gabriels.gabriel@uni9.edu.br>
2025-05-24 16:30:53 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function toggleTabela() {
+  const secao = document.getElementById("tabela");
+  if (secao.style.display === "none") {
+      secao.style.display = "block";
+  } else {
+      secao.style.display = "none";
+  }
+}
+
+console.log("Blog do Futebol carregado com sucesso!");
